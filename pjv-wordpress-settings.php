82a404e5a464224a791b22aed035c761302f8b36
Pieter-Jan Volders <pjvolders@gmail.com>
2013-06-26 16:40:20 +0200

    Remove testing code

--- pjv-wordpress-settings.php
+++ pjv-wordpress-settings.php
@@ -23,23 +23,4 @@ function load_pjv_settings_modules() {
 	do_action('pjv-settings-loaded');
 }
 
-function eg_settings_api_init() {
-
-	$tmp1 = new PJVSettingsPage('custom-settings-page', 'Eigen instellingen', 'Berschijving');
-	$sect = $tmp1->new_section('custom-settings-section', 'Mooie sectie');
-	$sect->new_setting('test_optie', 'Test optie', 'textfield', 'Wat een zever', 'Mooie beschrijving');
-	$sect->new_setting('test_optie_2', 'Klik voor shit', 'checkbox');
-	$sect->new_setting('test_optie_3', 'blabla', 'media');
-	$sect->new_setting('test_optie_4', 'Mooi zo', 'media', '', 'Beschrijving');
-	$sect->new_setting('optie_5', 'Kies maar iets', 'dropdown', null, '', array('aaa' => 'ja', 'bbb' => 'nee') );
-	$sect->new_setting('optie_7', 'Kies maar iets', 'radio', 'bbb', 'Haha leuk he', array('aaa' => 'ja', 'bbb' => 'nee') );
-	$sect->new_setting('optie_8', 'Grote optie', 'textarea', null, 'Mooie beschrijving');
-	
-}// eg_settings_api_init()
-add_action('pjv-settings-loaded', 'eg_settings_api_init');
-
-
-
-
-
 ?>
